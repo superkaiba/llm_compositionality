ad8c1ada73518e91cf955ef24dac19c9cd889a5c
superkaiba <thomasjiralerspong@gmail.com>
2024-08-10 21:25:36 -0400

    add plotting

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 from utils import load_results
 import matplotlib.pyplot as plt
-results = load_results("/home/mila/t/thomas.jiralerspong/llm_compositionality/scratch/llm_compositionality/results/pipeline_results_20240810_135811/EleutherAI")
+import pandas as pd
 def plot_results(results, save_dir):
 
     fig, ax = plt.subplots(1, 1)
@@ -16,7 +16,28 @@ def plot_results(results, save_dir):
     ax.set_ylabel('ID')
     plt.savefig(f"{save_dir}/results.png")  
     
-plot_results(results, "/home/mila/t/thomas.jiralerspong/llm_compositionality/scratch/llm_compositionality/results/pipeline_results_20240810_135811/EleutherAI")
 
+def plot_compositionality_over_time(csv_file, save_dir):
+    df = pd.read_csv(csv_file)
+    layer_numbers = df['layer_num'].unique()
+    fig, axes = plt.subplots(6, 4)
 
+    for layer_number in layer_numbers:
+        ax = axes[layer_number // 4, layer_number % 4]
+        layer_df = df[df['layer_num'] == layer_number]
+        n_words_correlated = layer_df['n_words_correlated'].unique()
+        for n_words in n_words_correlated:
+            n_words_correlated_df = layer_df[layer_df['n_words_correlated'] == n_words]
+            ax.plot(n_words_correlated_df['checkpoint_step'], n_words_correlated_df['id'], label=f"{n_words}")
 
+        ax.set_xlabel('Training epoch')
+        ax.set_ylabel('ID')
+        # ax.legend()
+        ax.set_title(f"Layer {layer_number}")
+        handles, labels = ax.get_legend_handles_labels()
+
+    fig.legend(handles, labels, loc='upper center', title='Number of correlated words', ncol=4)
+    fig.tight_layout()
+    fig.savefig(f"{save_dir}/compositionality_over_time.png")
+
+plot_compositionality_over_time("/home/mila/t/thomas.jiralerspong/llm_compositionality/results/pipeline_results_20240810_171441/EleutherAI_pythia-410m-dedupedresults.csv", "/home/mila/t/thomas.jiralerspong/llm_compositionality/results/pipeline_results_20240810_171441")
