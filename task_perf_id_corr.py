c74ae13470b23632abc6bd1da6c1cf9e1fe717ca
jinhl9 <haya.jinlee@gmail.com>
2024-08-22 00:53:56 +0100

    Updated with more checkpoints ID

--- task_perf_id_corr.py
+++ task_perf_id_corr.py
@@ -9,7 +9,7 @@ SUMMARY = pd.read_csv('task_performance_summary.csv')
 TASKS = SUMMARY['task'].unique()
 MODELS = SUMMARY['model_size'].unique()
 
-ID_MODELS = ['70m', '160m', '410m', '1b', '1.4b', '2.8b']
+ID_MODELS = ['70m', '160m', '410m', '1b', '1.4b', '2.8b', '6.9b']
 
 ALPHAS = {'70m': 0, '410m': 1, '1b': 2, '1.4b': 3, '2.8b': 4, '6.9b': 5}
 STEPS = SUMMARY['step'].unique()
@@ -96,41 +96,12 @@ def get_corr():
                                                       method=m,
                                                       layer_num=l,
                                                       corrcoef=corr_coef)
-                        """
-                        if all([
-                                i in performance_result.keys()
-                                for i in ckpt_inds.values
-                        ]):
-                            print('hey')
-                            perf_list = [
-                                performance_result[int(ckpt_ind)]
-                                for ckpt_ind in ckpt_inds
-                            ]
-                            corr_coef = np.corrcoef(id_list, perf_list)
-                            print('corr_coef')
-                            corr_results = append_results(
-                                corr_results,
-                                task=task,
-                                model_size=model_size,
-                                n_words_correlated=c,
-                                method=m,
-                                layer_num=l,
-                                corrcoef=corr_coef)
-                        
-                        else:
-                            missing = []
-                            for i in performance_result.keys():
-                                if i not in ckpt_inds.values:
-                                    missing.append(i)
-                            print(ckpt_inds.values)
-                            print(missing)
-                        """
+
     return corr_results
 
 
 ### Plot all the task evaluations
 def plot():
-
     if not os.path.isdir('results/task_performance'):
         os.mkdir('results/task_performance')
     for TASK in TASKS:
@@ -162,32 +133,9 @@ def plot():
                     pad_inches=3)
 
 
-corr_results = get_corr()
-df = pd.DataFrame(corr_results)
-df.to_csv("task_id_corr.csv")
-"""
+if __name__ == "__main__":
 
-fig, axs = plt.subplots(12, 6, figsize=(15, 10), sharex=True)
-
-ACC_TASKS = [t for t in TASKS if 'crows' not in t]
-for i, TASK in enumerate(ACC_TASKS):
-    for MODEL in MODELS:
-        result_dict = get_results(TASK, MODEL)
-        metric = 'acc'
-
-        sorted = np.argsort(list(result_dict[metric].keys()))
-        axs[i // 6][i % 6].errorbar(
-            np.array(list(result_dict[metric].keys()))[sorted],
-            np.array(list(result_dict[metric].values()))[sorted],
-            yerr=np.array(list(result_dict[metric +
-                                           '_stderr'].values()))[sorted],
-            marker='o',
-            ls='-',
-            label=MODEL,
-            color=colormap(normalize(ALPHAS[MODEL])),
-            ms=1)
-
-        axs[i // 6][i % 6].set_title(f'{i//10}, {i%10}')
-
-fig.savefig('test.png')
-"""
+    plot()
+    corr_results = get_corr()
+    df = pd.DataFrame(corr_results)
+    df.to_csv("task_id_corr.csv")
