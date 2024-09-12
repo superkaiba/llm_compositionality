389d54cbe4d22a197682809637483c63e5e411d9
jinhl9 <haya.jinlee@gmail.com>
2024-09-12 18:47:23 +0100

    Training dynamics figures

--- task_perf_id_corr.py
+++ task_perf_id_corr.py
@@ -14,6 +14,11 @@ ID_MODELS = ['70m', '160m', '410m', '1b', '1.4b', '2.8b', '6.9b']
 ALPHAS = {'70m': 0, '410m': 1, '1b': 2, '1.4b': 3, '2.8b': 4, '6.9b': 5}
 STEPS = SUMMARY['step'].unique()
 
+ID_METHODS = [
+    'pca_mean', 'pca_std', 'pr_mean', 'pr_std', 'twonn_mean', 'twonn_std',
+    'mle_mean', 'mle_std'
+]
+
 colormap = plt.cm.viridis  #or any other colormap
 normalize = matplotlib.colors.Normalize(vmin=0, vmax=5)
 
@@ -46,66 +51,124 @@ def get_results_id_perf():
         'acc_stderr': [],
         'likelihood_difference': [],
         'likelihood_difference_stderr': [],
-        'method': [],
-        'layer_num': [],
-        'ordered_id': [],
-        'shuffled_id': [],
-        'checkpoint_step': [],
+        'layer': [],
+        'words_coupled': [],
+        'mode': [],
+        'step': [],
         'model_size': [],
         'task': [],
         'shot': []
     }
-    for model_size in MODELS:
-        print(model_size)
-        id_result_ordered = pd.read_csv(
-            f'results/ordered/results_{model_size}.csv')
-        id_result_shuffled = pd.read_csv(
-            f'results/shuffled/results_{model_size}.csv')
-        for layer_num in id_result_ordered['layer_num'].unique():
-            for method in id_result_ordered['method'].unique():
-                for n in id_result_ordered['n_words_correlated'].unique():
+    for m in ID_METHODS:
+        result_dict[m] = []
+    id_result = pd.read_csv(f'results/emily_id_results_all.csv')
+    TASKS_SUB = [t for t in TASKS if 'crows' not in t and 'hendrycks' not in t]
+    STEPS_list = id_result['step'].unique()
+    for mode in id_result['mode'].unique():
+        print(mode)
+        for model_size in ['410m', '1.4b', '6.9b']:
+            print(model_size)
+            last_layer = max(
+                id_result[id_result['model'] == model_size]['layer'].unique())
+            for layer_num in range(last_layer):
+                print(layer_num)
+                for n in [1]:  #id_result['words_coupled'].unique():
                     for shot in [0, 5]:
-                        for task in TASKS:
-                            for step in STEPS:
+                        for task in TASKS_SUB:
+                            for step in STEPS_list:
                                 task_cond = (SUMMARY['task'] == task) & (
-                                    SUMMARY['model_size']
-                                    == model_size) & (SUMMARY['shot'] == shot)
+                                    SUMMARY['model_size'] == model_size) & (
+                                        SUMMARY['shot']
+                                        == shot) & (SUMMARY['step'] == step)
                                 id_cond = (
-                                    id_result_ordered['model_size']
-                                    == model_size
-                                ) & (id_result_ordered['checkpoint_step']
-                                     == step) & (
-                                         id_result_ordered['layer_num']
-                                         == layer_num) & (
-                                             id_result_ordered['method']
-                                             == method) & (id_result_ordered[
-                                                 'n_words_correlated'] == n)
-                                if len(id_cond) == 1:
+                                    id_result['model'] == model_size
+                                ) & (id_result['layer'] == layer_num) & (
+                                    id_result['step'] == step) & (
+                                        id_result['words_coupled']
+                                        == n) & (id_result['mode'] == mode)
+                                if sum(id_cond) == 1 and sum(task_cond) == 1:
+
                                     result_dict['shot'].append(shot)
                                     result_dict['model_size'].append(
                                         model_size)
                                     result_dict['task'].append(task)
                                     result_dict['step'].append(step)
+                                    result_dict['words_coupled'].append(n)
                                     result_dict['acc'].append(
-                                        SUMMARY[task_cond]['acc'])
+                                        SUMMARY[task_cond]['acc'].item())
+
                                     result_dict['acc_stderr'].append(
-                                        SUMMARY[task_cond]['acc_stderr'])
+                                        SUMMARY[task_cond]
+                                        ['acc_stderr'].item())
                                     result_dict[
                                         'likelihood_difference'].append(
                                             SUMMARY[task_cond]
-                                            ['likelihood_difference'])
+                                            ['likelihood_difference'].item())
                                     result_dict[
                                         'likelihood_difference_stderr'].append(
                                             SUMMARY[task_cond]
-                                            ['likelihood_difference_stderr'])
-                                    result_dict['layer_num'].append(layer_num)
-                                    result_dict['method'].append(method)
-                                    result_dict['ordered_id'].append(
-                                        id_result_ordered[id_cond]['id'])
-                                    result_dict['shuffled_id'].append(
-                                        id_result_shuffled[id_cond]['id'])
+                                            ['likelihood_difference_stderr'].
+                                            item())
+                                    result_dict['layer'].append(layer_num)
+                                    for m in ID_METHODS:
+                                        result_dict[m].append(
+                                            id_result[id_cond][m].item())
+                                    result_dict['mode'].append(
+                                        id_result[id_cond]['mode'].item())
 
     return result_dict
+
+
+def get_corr_new():
+
+    def append_results(df_dict, **kwargs):
+        for k, v in kwargs.items():
+            df_dict[k].append(v)
+        return df_dict
+
+    corr_results = {
+        'task': [],
+        'model_size': [],
+        'words_coupled': [],
+        'method': [],
+        'layer': [],
+        'corrcoef': [],
+        'mode': []
+    }
+    id_taskperf = pd.read_csv('task_id_new.csv')
+    methods = ['twonn_mean']  #[i for i in ID_METHODS if 'mean' in i]
+    for mode in id_taskperf['mode'].unique():
+        for task in id_taskperf['task'].unique():
+            for model_size in id_taskperf['model_size'].unique():
+                layers = id_taskperf['layer'][id_taskperf['model_size'] ==
+                                              model_size].unique()
+                for layer in layers:
+                    for method in methods:
+                        for n in id_taskperf['words_coupled'].unique():
+                            cond = (id_taskperf['model_size'] == model_size
+                                    ) & (id_taskperf['mode'] == mode) & (
+                                        id_taskperf['layer'] == layer) & (
+                                            id_taskperf['task'] == task
+                                        ) & (id_taskperf['words_coupled']
+                                             == n) & (id_taskperf['shot'] == 0)
+
+                            ckpt_inds = id_taskperf[cond]['step']
+                            sorted_ind = np.argsort(ckpt_inds).index
+                            perf_list = list(
+                                id_taskperf[cond]['acc'][sorted_ind])
+                            id_list = list(
+                                id_taskperf[cond][method][sorted_ind])
+                            corr_coef = np.corrcoef(id_list, perf_list)[0, 1]
+                            corr_results = append_results(
+                                corr_results,
+                                task=task,
+                                model_size=model_size,
+                                words_coupled=n,
+                                method=method,
+                                layer=layer,
+                                corrcoef=corr_coef,
+                                mode=mode)
+    return corr_results
 
 
 def get_corr(prompt_type="ordered"):
@@ -118,9 +181,9 @@ def get_corr(prompt_type="ordered"):
     corr_results = {
         'task': [],
         'model_size': [],
-        'n_words_correlated': [],
+        'coupled_words': [],
         'method': [],
-        'layer_num': [],
+        'layer': [],
         'corrcoef': []
     }
     RESULTS_PATH = f'results/{prompt_type}/results_'
@@ -135,16 +198,16 @@ def get_corr(prompt_type="ordered"):
                 continue
             id_result = pd.read_csv(RESULTS_PATH + f'{model_size}.csv')
             layer_nums = id_result['layer_num'].unique()
-            corr_words_n = id_result['n_words_correlated'].unique()
-            methods = id_result['method'].unique()
+            corr_words_n = id_result['words_coupled'].unique()
+            methods = [i for i in ID_METHODS if 'mean' in i]
             for l in layer_nums:
                 for c in corr_words_n:
                     for m in methods:
-                        cond = (id_result['n_words_correlated']
-                                == c) & (id_result['method']
-                                         == m) & (id_result['layer_num'] == l)
-                        ckpt_inds = id_result[cond]['checkpoint_step']
-                        id_list = id_result['id'][np.argsort(ckpt_inds).index]
+                        cond = (id_result['words_coupled']
+                                == c) & (id_result['layer'] == l)
+                        ckpt_inds = id_result[cond]['step']
+                        id_list = id_result[methods][np.argsort(
+                            ckpt_inds).index]
                         perf_list = []
                         for i in ckpt_inds.values:
                             if i in performance_result.keys():
@@ -254,9 +317,11 @@ def plot_perf_ID():
 if __name__ == "__main__":
 
     #plot()
-    #corr_results = get_corr()
-    #df = pd.DataFrame(corr_results)
-    #df.to_csv("task_id_corr_ordered_five.csv")
-    task_perf_id = get_results_id_perf()
-    df = pd.DataFrame(task_perf_id)
-    df.to_csv("task_id_merged.csv")
+
+    #task_perf_id = get_results_id_perf()
+    #df = pd.DataFrame(task_perf_id)
+    #df.to_csv("task_id_new.csv")
+
+    corr_results = get_corr_new()
+    df = pd.DataFrame(corr_results)
+    df.to_csv("task_id_corr_new.csv")
